1eae09fe8ea03c33bc3ed96e7a1f9c3519364a61
fa <furkanalaybeg@gmail.com>
2017-12-13 17:56:35 +0300

    Chapter 3 - Exercise 1 is finished. There have been done some modifies by "add-migration" command.

--- ExamplesApp/ExamplesApp/Migrations/Configuration.cs
+++ ExamplesApp/ExamplesApp/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace ExamplesApp.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(ExamplesApp.Models.ApplicationDbContext context)
