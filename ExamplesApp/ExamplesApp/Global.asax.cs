4866738e9621d4901c3fecf70efb51e0ae0e52b3
fa <furkanalaybeg@gmail.com>
2017-12-22 16:21:47 +0300

    Chapter 3 - Exercise 1 has been finished. Web API Controller for Customer and Movie were created. Also DTO model for Web API was created.

--- ExamplesApp/ExamplesApp/Global.asax.cs
+++ ExamplesApp/ExamplesApp/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using ExamplesApp.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,10 +15,13 @@ namespace ExamplesApp
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c=>c.AddProfile<MappingProfile>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+          
         }
     }
 }
